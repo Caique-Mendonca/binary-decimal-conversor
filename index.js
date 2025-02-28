793b7fa7f69c687de8990a82894dbd627c0a4991
Caique-Mendonca <caique4874@gmail.com>
2025-02-28 20:59:20 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 document.querySelector("#valor-binario-1").addEventListener('keydown', (event)=>{
     if (!["0", "1", "Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(event.key)) {
-        event.preventDefault(); // Bloqueia a tecla
+        event.preventDefault();
       }
+})
+let $botaoCalcularBinDec = document.querySelector("#botao-calcular-bin-dec")
+$botaoCalcularBinDec.addEventListener('click',(event)=>{
+  event.preventDefault
+  let $decimal1 = document.querySelector("#valor-decimal-1")
+  let binario1 = document.querySelector("#valor-binario-1").value
+
+  let peso = 0
+  let binario1array = []
+
+  let binario1lista = binario1.split("").map(Number).reverse()
+  while (peso < binario1lista.length) {
+    let binarioOperacao = binario1lista[peso]* (2**peso)
+    binario1array.push(binarioOperacao)
+    peso ++
+    console.log(`binario1 array ${binario1array}`);
+  }
+
+  let numeroDecimalConvertido = binario1array.reduce((somador , numero)=> somador += numero,0)
+
+  $decimal1.value = numeroDecimalConvertido
 })
